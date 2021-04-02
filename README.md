07abee003d36723c9865061f48e25562d8f09dfe
Olga <zhuravel.olga95@gmail.com>
2021-04-02 21:03:32 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Страницы
+
+http://m99935yr.beget.tech/worldApartments
+http://m99935yr.beget.tech/worldApartments/catalogy.html
+http://m99935yr.beget.tech/worldApartments/ads.html
+
+# Стартовый шаблон
+Используется связка `Gulp` + `Pug` + `SASS` + `BrowserSync` + `Linters`
+
+## Установка
+Склонировать репозиторий и выполнить npm install
+
+## Start 
+gulp
+
---
+++ dev/js/main.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+
+  // tab
+  $(".tab").click(function () {
+    let index = $(this).index();
+    $(this).addClass('active').siblings().removeClass('active');
+    $(".form").eq(index).addClass('active').siblings().removeClass('active');
+  });
+
+  // stopPropagation
+  $(".stopPropagation").click(function (e) {
+    e.stopPropagation();
+  });
+
+  // select
+  // $(".select-item").click(function () {
+  //   $(this).toggleClass('active').parents().siblings().find(".select-item").removeClass('active');
+  //   $(this).siblings(".select-drop").slideToggle(300).parents().siblings().find(".select-drop").slideUp(300);
+  // });
+  // $("html, body").click(function () {
+  //   $(".select-item").removeClass('active');
+  //   $(".select-drop").slideUp(300);
+  // });
+  // $(".select-option").click(function () {
+  //   let optionText = $(this).text();
+  //   $(this).addClass('active').siblings().removeClass('active');
+  //   $(this).closest(".select").find(".select-item span").text(optionText);
+  //   $(".select-item").removeClass('active');
+  //   $(".select-drop").slideUp(300);
+  // });
+
+
+});
