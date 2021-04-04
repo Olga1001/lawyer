55959d7e7d30ba369e9ea8e09db6a974e31f7563
Olga <zhuravel.olga95@gmail.com>
2021-04-04 16:32:12 +0300

    commit

--- dist/js/main.js
+++ dist/js/main.js
@@ -10,20 +10,9 @@ $(document).ready(function () {
 
   $(".stopPropagation").click(function (e) {
     e.stopPropagation();
-  }); // select
-  // $(".select-item").click(function () {
-  //   $(this).toggleClass('active').parents().siblings().find(".select-item").removeClass('active');
-  //   $(this).siblings(".select-drop").slideToggle(300).parents().siblings().find(".select-drop").slideUp(300);
-  // });
-  // $("html, body").click(function () {
-  //   $(".select-item").removeClass('active');
-  //   $(".select-drop").slideUp(300);
-  // });
-  // $(".select-option").click(function () {
-  //   let optionText = $(this).text();
-  //   $(this).addClass('active').siblings().removeClass('active');
-  //   $(this).closest(".select").find(".select-item span").text(optionText);
-  //   $(".select-item").removeClass('active');
-  //   $(".select-drop").slideUp(300);
-  // });
+  });
+  $(".burger").click(function () {
+    $(this).toggleClass('active');
+    $(".navmenu").toggleClass('active');
+  });
 });
